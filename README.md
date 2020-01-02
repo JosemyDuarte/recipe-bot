37f24e8007d617f81b2adcc71839da9dbcd59f68
Josemy Duarte <duartejosemy@gmail.com>
2020-01-02 22:23:34 +0000

    Added recipes

---
+++ README.md
@@ -0,0 +1,23 @@
+# Recipe Bot
+
+There is a bot for everything, and this is a bot for recipes. It was build making use of [Serverless Framework CLI](https://serverless.com/framework/docs/getting-started/). Why? Just for fun 😉
+
+## How it works?
+
+First, just start a conversation with the bot with a `/start` command/message. After that just send a message like `/recipe [Ingredient1, ingredient2 ...]`. 
+
+![screenshot](./TelegramScreenshot.png)
+
+## Do you wanna try it?
+
+The main thing you will require is a Telegram Bot Token. Once you have that and with [Docker](https://www.docker.com/) installed, build the image:
+
+`docker build --rm -f "Dockerfile" -t recipebot:latest "."`
+
+And run the Serverless Server 😉:
+
+`docker run -e TELEGRAM_TOKEN=<YOUR_TELEGRAM_TOKEN> -it --rm -p 80:80 recipebot`
+
+Now you only need to use some tunneling tool like [localtunnel](https://github.com/localtunnel/localtunnel) or [ngrok](https://ngrok.com/) and set the url as a webhook to Telegram making a request to this URL:
+
+`https://api.telegram.org/bot<YOUR_TOKEN_BOT>/setWebhook?url=<TUNNELING_URL>/find`
--- handler.js
+++ handler.js
@@ -1,28 +1,36 @@
 "use strict";
 
-const getRecipe = async () => {
+const getRecipe = async (ingredients) => {
   try {
     const axios = require('axios').default;
-    return await axios.get("http://www.recipepuppy.com/api/", {
-      params: { i: "onions, garlic", q: "omelet", p: "2" }
-    })
+    const recipe = await axios.get("http://www.recipepuppy.com/api/", {
+      params: { i: ingredients, p: "1" }
+    });
+    console.log(recipe.data);
+    return recipe;
   } catch (error) {
     console.error(error)
   }
 }
+
+
 module.exports.hi = async function (event, context) {
-  const recipeResponse = await getRecipe()
+  const recipeResponse = await getRecipe("eggs, cheese")
   const response = {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World!', event: event, recipe: recipeResponse.data }),
+    body: JSON.stringify({ message: 'Hello World!', event: event, recipe: recipeResponse.data, }),
   };
   return response
 };
 
 
 exports.find = async (event) => {
-  const Telegraf = require('telegraf')
+  const Telegraf = require('telegraf');
   const bot = new Telegraf(process.env["TELEGRAM_TOKEN"]);
+  const urlExists = require('url-exists');
+  const commandArgsMiddleware = require('./commandArgs');
+  bot.use(commandArgsMiddleware());
+
   bot.use(async (ctx, next) => {
     const start = new Date()
     await next()
@@ -30,9 +38,27 @@ exports.find = async (event) => {
     console.log('Response time: %sms', ms)
   })
 
-  bot.command('recipe', async ({ reply }) => {
-    getRecipe()
-      .then(response => reply(JSON.stringify(response.data)))
+  bot.start((ctx) => ctx.reply('Hello'))
+
+  bot.command('recipe', (ctx) => {
+    getRecipe(ctx.state.command.args.join())
+      .then(response => {
+        const recipes = response.data.results ? response.data.results : [];
+        if (recipes.length > 0) {
+          recipes.slice(-5).forEach(recipe => {
+            urlExists(recipe.href, function (err, exists) {
+              if (exists) {
+                ctx.reply(recipe.title.trim().replace(/[^a-zA-Z ]/g, "").concat(": ", recipe.href))
+              }
+              else {
+                console.log("Ignoring:\n ".concat(JSON.stringify(recipe)));
+              }
+            })
+          });
+        } else {
+          ctx.reply("No recipe found for ".concat(ctx.state.command.args.join()))
+        }
+      })
       .catch(error => {
         console.log(error)
       })
